f043e8494a7e01c4e3d79d7fc9fcaafef81dd817
Diego <20101010@ue.edu.pe>
2023-10-24 04:22:00 -0500

    Adding a login page with svg and png files.

--- src/router/routes.js
+++ src/router/routes.js
@@ -8,6 +8,11 @@ const routes = [
     ]
   },
 
+  {
+    path: '/login',
+    component: () => import('pages/MyLogin.vue')
+  },
+
   // Always leave this as last one,
   // but you can also remove it
   {
